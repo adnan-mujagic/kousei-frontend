0dbb31e166ff9e1cb2ce81b5de9602ca44950f40
adnan-mujagic <adnanmujagic@outlook.com>
2021-07-15 14:52:43 +0200

    LoadingBar added - not smooth though

---
+++ kousei/src/components/LoadingBar/LoadingBar.js
@@ -0,0 +1,9 @@
+import "./LoadingBar.css";
+
+export default function LoadingBar(){
+    return (
+        <div className="Loading-bar">
+            <div className="Inside-loading-bar"></div>
+        </div>
+    )
+}
